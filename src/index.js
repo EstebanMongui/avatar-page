be4f44a7ea66306fc3732f0180ff67105d5e35c7
Esteban Monguí <esteband.itorres@gmail.com>
2020-12-11 14:21:54 -0500

    refact: update initial settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //React
 import React from 'react';
 import ReactDOM from 'react-dom';
+//Pages
+import MovieDetail from "./pages/MovieDetail.js"
 
 // Containers
 const container = document.getElementById('root')
 
-ReactDOM.render(<Page/>, container)
+ReactDOM.render(<MovieDetail/>, container)
